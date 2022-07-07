de3a2be64d94ddb6024f68db298c46d6b62d07de
Dima Tytenko <amgpetronass@gmail.com>
2022-07-07 21:35:27 +0300

    add fn public and private routes

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { authSelectors } from '../redux/auth';
+
+export function PrivateRoute({ children, ...routeProps }) {
+  const isLoggedIn = useSelector(
+    authSelectors.getIsLoggedIn
+  );
+  return (
+    <>
+      {isLoggedIn ? (
+        children
+      ) : (
+        <Navigate to="/login" replace={false} />
+      )}
+    </>
+  );
+}
---
+++ src/routes/PublicRoute.js
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { authSelectors } from '../redux/auth';
+
+export function PublicRoute({
+  children,
+  restricted = false,
+  redirectTo = '/',
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(
+    authSelectors.getIsLoggedIn
+  );
+  const shouldRedirect = isLoggedIn && restricted;
+  return (
+    <>
+      {shouldRedirect ? (
+        <Navigate to={redirectTo} replace={false} />
+      ) : (
+        children
+      )}
+    </>
+  );
+}
