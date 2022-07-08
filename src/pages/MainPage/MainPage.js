f1c4b311f692cef90748621bcc8ecf98f7fda165
alionakaliuzhnaya <alionakaliuzhnaya@icloud.com>
2022-07-08 17:34:18 +0200

    main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,12 +2,16 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import { Container } from '../../components/Container';
+import {MainBox} from "./MainPage.styled"
 
 export default function MainPage(props) {
   return (
+    <>
+    <MainBox>
     <Container>
-      <div>MainPage</div>
     </Container>
+      </MainBox>
+    </>
   );
 }
 
