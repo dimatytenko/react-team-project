fc342135b3d7298c996371021659e12851b87337
Dima Tytenko <amgpetronass@gmail.com>
2022-07-08 11:54:10 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { useEffect, Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Global } from '@emotion/react';
 import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
@@ -8,6 +10,22 @@ import { themeSelectors } from '../../redux/theme';
 import { Container } from '../Container';
 import { SwitchTheme } from '../SwitchTheme';
 
+// const { MainPage } = lazy(() =>
+//   import('../../pages/MainPage')
+// );
+// const { LoginPage } = lazy(() =>
+//   import('../../pages/LoginPage')
+// );
+// const { RegistrationPage } = lazy(() =>
+//   import('../../pages/RegistrationPage')
+// );
+// const { DiaryPage } = lazy(() =>
+//   import('../../pages/DiaryPage')
+// );
+// const { CalculatorPage } = lazy(() =>
+//   import('../../pages/CalculatorPage')
+// );
+
 export function App() {
   const isTheme = useSelector(themeSelectors.getTheme);
   return (
@@ -16,10 +34,14 @@ export function App() {
         theme={!isTheme ? ThemeLight : ThemeDark}
       >
         <Global styles={GlobalStyles} />
+
         <Container>
-          <h1>Hi team</h1>
           <SwitchTheme />
         </Container>
+
+        <Suspense fallback={<p>download...</p>}>
+          <Routes></Routes>
+        </Suspense>
       </ThemeProvider>
     </>
   );
