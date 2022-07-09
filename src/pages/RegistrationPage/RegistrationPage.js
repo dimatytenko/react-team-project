7831c389c965b576783e5eacb83bdb9ca8a2f7a4
Dima Tytenko <amgpetronass@gmail.com>
2022-07-09 20:47:42 +0300

    fix path

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Container } from '../../components/Container';
-import { MainPage } from '../MainPage/MainPage.styled';
+import { MainBox } from '../MainPage/MainPage.styled';
 
 export default function RegistrationPage(props) {
   return (
-    <MainPage>
+    <MainBox>
       <Container>
         <div> RegistrationPage</div>
       </Container>
-    </MainPage>
+    </MainBox>
   );
 }
 
