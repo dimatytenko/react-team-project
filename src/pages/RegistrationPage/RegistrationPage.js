72a8b8d369d2666bce5af2de709310c67c8ad4bc
Dima Tytenko <amgpetronass@gmail.com>
2022-07-09 20:26:45 +0300

    add back register

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Container } from '../../components/Container';
-import MainPage from '../MainPage/MainPage';
+import { MainPage } from '../MainPage/MainPage.styled';
 
 export default function RegistrationPage(props) {
   return (
-    <>
+    <MainPage>
       <Container>
         <div> RegistrationPage</div>
       </Container>
-    </>
+    </MainPage>
   );
 }
 
