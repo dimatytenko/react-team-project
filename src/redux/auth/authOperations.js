52912e8cd46698b2f01af1d9216fff7ff96cd92f
alionakaliuzhnaya <alionakaliuzhnaya@icloud.com>
2022-07-09 18:18:46 +0200

    add background logIn loginform

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,6 +3,19 @@ import * as connectionsAPI from '../../services/connectionsAPI';
 
 const token = connectionsAPI.token;
 
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (userData) => {
+    try {
+      const result = await connectionsAPI.fetchLogIn(userData);
+      token.set(userData.token);
+      return result;
+    } catch (error) {
+      return error.rejectWithValue();
+    }
+  }
+);
+
 export const logOut = createAsyncThunk(
   'auth/logout',
   async () => {
