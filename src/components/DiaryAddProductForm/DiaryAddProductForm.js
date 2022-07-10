2053bba3e65d8561096545ca81aee3df2226604c
Dima Tytenko <amgpetronass@gmail.com>
2022-07-10 15:19:45 +0300

    start product form

--- src/components/DiaryAddProductForm/DiaryAddProductForm.js
+++ src/components/DiaryAddProductForm/DiaryAddProductForm.js
@@ -1,43 +1,48 @@
 // import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
-import TextField from '@mui/material/TextField';
+import { TextField, Input } from '@mui/material';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8080/api';
 
 export function DiaryAddProductForm(props) {
   const [productValue, setProductValue] = useState('');
-  console.log(productValue);
+  const [titles, setTitles] = useState([]);
 
-  // async function fetchProducts(product) {
-  //   const data = await axios
-  //     .get('/products', product)
-  //     .then(res => res.data);
-  //   return data;
-  // }
+  console.log(titles);
+
   useEffect(() => {
     if (productValue === '') {
       return;
     }
-    async function fetchProducts() {
-      const data = await axios
-        .get('/products')
+    async function fetchProducts(productValue) {
+      const { data } = await axios
+        .get(`/products?search=${productValue}`)
         .then(res => res.data);
-      return data;
+      const titleArray = await data.products.map(
+        obj => obj.title.en
+      );
+      await setTitles(titleArray);
+      return titleArray;
     }
 
-    fetchProducts();
+    fetchProducts(productValue);
   }, [productValue]);
 
   return (
     <div>
-      <TextField
+      <Input
         onChange={e => setProductValue(e.target.value)}
+        placeholder="Placeholder"
+        inputProps={productValue}
+      />
+
+      {/* <TextField
         id="standard-search"
         label="Search field"
         type="search"
         variant="standard"
-      />
+      /> */}
     </div>
   );
 }
--- src/pages/DiaryPage/DiaryPage.js
+++ src/pages/DiaryPage/DiaryPage.js
@@ -2,11 +2,16 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import { Container } from '../../components/Container';
+import { DiaryAddProductForm } from '../../components/DiaryAddProductForm';
+import { MainWrapper } from '../MainPage/MainPage.styled';
 
 export default function DiaryPage(props) {
   return (
     <Container>
-      <div>DiaryPage</div>
+      <MainWrapper>
+        <div>DiaryPage</div>
+        <DiaryAddProductForm />
+      </MainWrapper>
     </Container>
   );
 }
