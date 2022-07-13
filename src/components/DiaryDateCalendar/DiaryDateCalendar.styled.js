439b135631cd086e5cc4576a8388f4e163314ce5
Anna Samsonenko <foster.anna93@gmail.com>
2022-07-14 00:03:30 +0300

    fixes

--- src/components/DiaryDateCalendar/DiaryDateCalendar.styled.js
+++ src/components/DiaryDateCalendar/DiaryDateCalendar.styled.js
@@ -28,36 +28,23 @@ export const CalendarWrapper = styled.div`
 export const Calendar = styled(DatePicker)`
   & .react-date-picker__wrapper {
     border: none;
-
     ${mediaTablet(`
     align-items: baseline;
    `)}
   }
-
   & .react-date-picker__inputGroup {
     display: none;
   }
-
-  /* & .react-date-picker__inputGroup,
-  & .react-date-picker__inputGroup__leadingZero {
-    box-sizing: border-box;
-    font-family: 'Verdana';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 1.22;
-    color: ${props => props.theme.fontColors.secondary};
-
-    ${mediaTablet(`
-    font-size: 34px;
-    line-height: 41px;
-   `)}
-  } */
+  &
+    .react-date-picker__calendar.react-date-picker__calendar--open {
+    left: 0px !important;
+    min-width: 320px;
+    transform: translateX(-45.5%);
+  }
 
   & .react-date-picker__inputGroup__input {
     color: ${props => props.theme.fontColors.secondary};
   }
-
   & .react-date-picker__button {
     padding: 0;
     line-height: 0;
