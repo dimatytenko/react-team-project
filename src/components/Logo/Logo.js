3a64e8a2894239279e98c1321095ffd653993f1e
ITigor1985 <xromer85@gmail.com>
2022-07-13 14:55:53 +0300

    fix

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { authSelectors } from '../../redux/auth';
+
 import {
   LogoContainer,
   LogoMom,
   LogoSlim,
   LogoFirst,
 } from './Logo.styled';
+import useWindowDimensions from '../../customHooks/useWindowDimensions';
 
 export const Logo = () => {
+  const { width } = useWindowDimensions();
+  const isLoggedIn = useSelector(
+    authSelectors.getIsLoggedIn
+  );
+
   return (
     <Link to="/">
       <LogoContainer>
         <LogoFirst></LogoFirst>
-        <LogoSlim></LogoSlim>
-        <LogoMom></LogoMom>
+        {(isLoggedIn && <LogoSlim></LogoSlim>) ||
+          (!isLoggedIn && width > 768 && (
+            <LogoSlim></LogoSlim>
+          ))}
+        {(isLoggedIn && <LogoMom></LogoMom>) ||
+          (!isLoggedIn && width > 768 && (
+            <LogoMom></LogoMom>
+          ))}
       </LogoContainer>
     </Link>
   );
