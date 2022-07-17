c6b83a2ca2ee460a487f9dff51498fbe1326789d
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-07-17 21:50:20 +0300

    calculator

--- src/pages/CalculatorPage/CalculatorPage.js
+++ src/pages/CalculatorPage/CalculatorPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchCurrentUser } from '../../redux/auth/authOperations';
 // import PropTypes from 'prop-types';
 import { authSelectors } from '../../redux/auth';
 
@@ -18,25 +18,42 @@ import { RightSideBar } from '../../components/RightSideBar/RightSideBar';
 import { fetchUserData } from '../../services/connectionsAPI';
 
 export default function CalculatorPage(props) {
+  const dispatch = useDispatch();
   const isLoggedIn = useSelector(
     authSelectors.getIsLoggedIn
   );
+  const userData = useSelector(authSelectors.getUserData);
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const [calculatingData, setCalculatingData] = useState(
-    () => {
-      return (
-        JSON.parse(
-          window.localStorage.getItem('calculatingData')
-        ) ?? {}
-      );
-    }
+  const parsedData = JSON.parse(
+    window.localStorage.getItem('calculatingData')
   );
+  const isLsDataAvailable =
+    Object.keys(parsedData).length !== 0;
+  const calculatingData = isLsDataAvailable
+    ? parsedData
+    : userData;
 
   const calculatorSubmitHandler = async calculatingData => {
     try {
       setIsLoading(true);
       await fetchUserData(calculatingData);
+      const {
+        height,
+        age,
+        weight_current,
+        weight_desired,
+        blood,
+      } = calculatingData;
+      dispatch(
+        fetchCurrentUser({
+          height,
+          age,
+          weight_current,
+          weight_desired,
+          blood,
+        })
+      );
       setIsLoading(false);
       window.localStorage.setItem(
         'calculatingData',
@@ -54,11 +71,13 @@ export default function CalculatorPage(props) {
         <Container>
           <UserPagesBackWrapper>
             <CalculatorWrapper>
-              <DailyCaloriesForm
-                onFormSubmit={calculatorSubmitHandler}
-                data={calculatingData}
-                isLoading={isLoading}
-              />
+              {isLoggedIn && (
+                <DailyCaloriesForm
+                  onFormSubmit={calculatorSubmitHandler}
+                  data={calculatingData}
+                  isLoading={isLoading}
+                />
+              )}
               {isLoggedIn && <RightSideBar />}
             </CalculatorWrapper>
           </UserPagesBackWrapper>
